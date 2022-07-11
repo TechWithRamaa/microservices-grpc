b8ec4fba06c2e5a4049b10e463514c9ae2252ebb
EngineeringWithRamaa <rkramadhevi@gmail.com>
2022-07-11 20:34:33 +0530

    gRPC IDL, Protobuf, Stubs generated

---
+++ src/test/java/com/engineeringwithramaa/microservicesgrpc/MicroservicesGrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engineeringwithramaa.microservicesgrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesGrpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
